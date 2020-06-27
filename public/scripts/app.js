100bf1467b5acd426d652bcbe1a735010c453352
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-27 19:27:17 +0000

    Complete appending(need to style)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,34 +8,66 @@ $(() => {
     }
   });;
 
-  const createAccountElement = (accountData) => {
-       const $account = $(`
-       <article class="account">
-          <h3>${accountData.websites}</h3> <!-- stretch to change username-->
-          <h3 >Website</h3>
+  // <section id="category" <!-- display website info -->
+  //       <h2>Catagories</h2>
+  //       <article class="row-beside" id="accountContainer">
+  //         <div class="row-beside">
+  //           <h4>image</h4>
+  //           <div>
+  //             <h3>Website</h3>
+  //             <h3>Username</h3> <!-- stretch to change username-->
+  //           </div>
+  //        </div>
+  //        <form class="row-beside">
+  //          <h3 id='editPassword'>Password</h3> <!-- toggle or on hover (or the eye thing)-->
+  //          <button id='copy'>Copy</button>
+  //        </form>
+  //         <form>
+  //           <button id='edit'>Edit</button>
+  //           <button id='delete'>Delete</button>
+  //         </form>
+  //       </article>
+  //     </section>
 
-          <form method="POST" action="/websites/26/delete">
-            <h3 id='editPassword'>Password</h3> <!-- toggle or on hover (or the eye thing)-->
-            <button id='copy'>Copy</button>
-            <button id='edit'>Edit</button>
-            <button id='delete'>Delete</button>
-          </form>
-          `)
+  // const createAccountElement = (accountData) => {
+  //      const $account = $(`
+  //      <section class="account" style="margin: 5vh 0; border: 2px solid black">
+  //      <div class="row-beside">
 
-    return $account;
-  }
+  //        <h4>image</h4>
+  //        <div>
+  //          <h3>${accountData.url}</h3>
+  //          <h3>${accountData.username}</h3> <!-- stretch to change username-->
+  //        </div>
+  //     </div>
+  //     <form class="row-beside">
+  //       <h3 id='editPassword'>${accountData.password}</h3> <!-- toggle or on hover (or the eye thing)-->
+  //       <button id='copy'>Copy</button>
+  //     </form>
+  //      <form>
+  //        <button id='edit'>Edit</button>
+  //        <button id='delete'>Delete</button>
+  //      </form>
+  //    </section>
+  //         `)
+
+  //   return $account;
+  // }
 
   const renderAccounts = (accounts) => {
     for (const account in accounts) {
-      const $account = createAccountElement(account)
+      console.log(accounts[account])
+      for (const acc in  account) {
+        const $account = createAccountElement(accounts[account][acc])
       $('#accountContainer').prepend($account)
+      }
+
     }
   }
 
   const loadAccounts = () => {
     $.getJSON('/websites')
       .then(function (data) {
-        console.log(data)
         $('#account').empty();
         renderAccounts(data);
       })
