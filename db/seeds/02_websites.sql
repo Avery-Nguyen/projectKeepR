3c70677864d677a3e217fdf6acd7bf7c459b5f97
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-27 02:15:18 +0000

    fixed sql syntax and added wireframe to html

--- db/seeds/02_websites.sql
+++ db/seeds/02_websites.sql
@@ -1,5 +1,5 @@
 INSERT INTO websites (user_id, url, password, username)
-VALUES (1, 'https://www.facebook.com/', 'H3ll0', 'XxalicexX')
+VALUES (1, 'https://www.facebook.com/', 'H3ll0', 'XxalicexX'),
 (2, 'https://ca.linkedin.com/', 'W0rld', '123Avery'),
 (3, 'https://github.com/', 'ST3v3e', 'carrie_underwood'),
 (4, 'https://www.lighthouselabs.ca/', 'JoBs', 'dwarfMaster'),
