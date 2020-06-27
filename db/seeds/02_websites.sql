9a495c290c170637cec93eaa1fab887cac20d203
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-27 03:10:11 +0000

    Add category column to websites table

--- db/seeds/02_websites.sql
+++ db/seeds/02_websites.sql
@@ -1,4 +1,4 @@
-INSERT INTO websites (user_id, url, password, username, categories)
+INSERT INTO websites (user_id, url, password, username, category)
 VALUES (1, 'https://www.facebook.com/', 'H3ll0', 'XxalicexX', 'social'),
 (2, 'https://ca.linkedin.com/', 'W0rld', '123Avery', 'work'),
 (3, 'https://github.com/', 'ST3v3e', 'carrie_underwood', 'work'),
